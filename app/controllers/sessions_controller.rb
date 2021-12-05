99d902542116c951a500eb95578e03a3055f8045
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-05 10:11:52 -0600

    fix bugs in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,9 @@
 class SessionsController < ApplicationController
 
   def create
-    byebug
-    user = User.find_by(email: params[:user][:id])
-    if user && user.authenticate(params[:user][:password])
+    # byebug
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
       render json: user.as_json(options)
     else 
       render json: {error: 'SOmething went wrong! Please try again. Thanks.'}
