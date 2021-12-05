fa8f56d3fef3d4849674f6faa4af6a1a63d99641
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-04 18:32:31 -0600

    add sessions controller and setup routes for it

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,34 @@
+class SessionsController < ApplicationController
+
+  def create
+    user = User.find_by(email: params[:user][:id])
+    if user && user.authenticate(params[:user][:password])
+      render json: user.as_json(options)
+    else 
+      render json: {error: 'SOmething went wrong! Please try again. Thanks.'}
+    end
+  end
+
+  def destroy
+  end
+
+
+  private 
+
+  def options
+    {
+      except: [:created_at, :updated_at],
+      include: 
+        {weights: 
+          {except: [:created_at, :updated_at]},
+        trening_sessions: 
+          {except: [:created_at, :updated_at]},
+        weight_goal: {},
+        trening_goals:{}
+        }
+    }
+  end
+
+  def sessions_params
+  end 
+end
